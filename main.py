5540b596cc1e005724b91d0f2964012bd9702f8e
GandalfTheGray <joseph.m.siebold@gmail.com>
2025-03-19 12:02:30 -0500

    First game loop

---
+++ main.py
@@ -0,0 +1,32 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame, constants
+screen_width = constants.SCREEN_WIDTH
+screen_height = constants.SCREEN_HEIGHT
+game_on = True
+
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+def game_loop():
+    while game_on == True:
+        pygame.display.flip() # builds the game screen
+
+        for event in pygame.event.get(): # In the event pygame.event.get is called
+            if event.type == pygame.QUIT: # and the event.type is pygame.quit - quit the game
+                return
+
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {screen_width}")
+    print(f"Screen height: {screen_height}")
+    pygame.init()
+    game_loop()
+        
+    
+
+
+
+if __name__ == "__main__":
+    main()
