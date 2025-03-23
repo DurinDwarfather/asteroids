3749d9d9bcc11fe9b64ef04bce6052c0f5ec9a93
GandalfTheGray <joseph.m.siebold@gmail.com>
2025-03-22 23:05:14 -0500

    Added astroid class and astroid field

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame, constants, circleshape
-import player
+import pygame, constants, circleshape, player
+
+#defines object grouping
+updatable = pygame.sprite.Group()
+drawables = pygame.sprite.Group()
+
+
+#defines group membership
+player.Player.containers = (updatable, drawables)
+
+game_on = True #creates the run condition for the while loop.
+dt = 0 # creates the detla time variable to be used elseware. 
+refresh_rate = 60 # sets the game clock refresh rate 
+clock = pygame.time.Clock() #the game clock
+
+#define objects that will appear on the screen
 screen_width = constants.SCREEN_WIDTH
 screen_height = constants.SCREEN_HEIGHT
-game_on = True
-dt = 0
-refresh_rate = 60
-clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
 x = constants.SCREEN_WIDTH / 2
 y = constants.SCREEN_HEIGHT / 2
 player_object = player.Player(x, y)
 
-
 def game_loop():
     global dt
     while game_on == True:
         dt = clock.tick(60) / 1000.0 # convert miliseconds to seconds
-        
+
         screen.fill("black") # fills screen with black background
-        player_object.draw(screen) # draws the player on the background
-        player_object.update(dt) # allows the player to rorate left and right
+        for dawn in drawables:
+            dawn.draw(screen) # draws the player on the background
+        updatable.update(dt) # allows the player to rorate left and right
         pygame.display.flip() # refreshes the game screen
 
         for event in pygame.event.get(): # In the event pygame.event.get is called
@@ -34,7 +44,7 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {screen_width}")
     print(f"Screen height: {screen_height}")
-    # Initiate game loop
+    # Initiate game loop``
     pygame.init()
     game_loop()
         
